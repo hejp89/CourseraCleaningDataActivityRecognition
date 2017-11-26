29cf119fb7340a11aa0e8c5f4851c02768e4dd81
Howard Paget <hejp89@users.noreply.github.com>
2017-11-26 12:09:42 +0000

    Added the code book summary and comments to the code

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(dplyr)
-library(reshape2)
+
+# Read in the required space (" ") separated text files
 
 features <- fread("UCI HAR Dataset/features.txt", sep = " ", stringsAsFactors=FALSE)
 activity_labels <- fread("UCI HAR Dataset/activity_labels.txt", sep = " ", stringsAsFactors=FALSE)
@@ -14,17 +15,26 @@ Y_test <- fread("UCI HAR Dataset/test/Y_test.txt", sep = " ")
 subject_train <- fread("UCI HAR Dataset/train/subject_train.txt")
 subject_test <- fread("UCI HAR Dataset/test/subject_test.txt")
 
+# Select only the columns that contain the mean and standard deviation of the measurements
+
 mean_std <- which(grepl("mean|std", features$V2))
 X_train <- X_train[, ..mean_std]
 X_test <- X_test[, ..mean_std]
 
+# Combine the subject, activity, and features data frames for both train and test into a single data frame
+
 all_data <- rbind(cbind(subject_train, Y_train, X_train), cbind(subject_test, Y_test, X_test))
 names(all_data) <- c("subject", "activity", unlist(features[mean_std, 2]))
+
+# Replace the numeric values in the activity column with string descriptions i.e. replace all 1s with "WALKING", etc
 
 all_data$activity <- factor(all_data$activity, levels = unlist(activity_labels[,1]), labels = unlist(activity_labels[,2]))
 all_data$subject <- as.factor(all_data$subject)
 
-all_data <- melt(all_data, id = c("subject", "activity"))
-all_data <- dcast(all_data, subject + activity ~ variable, mean)
+# Calculate the mean of all of the measurements grouped by subject and activity
 
-write.table(all_data, "tidy.txt", row.names = FALSE, quote = FALSE)
+tidy <- all_data %>% group_by(subject, activity) %>% summarize_all(funs(mean))
+
+# Write the tidy data to "tidy.txt" 
+
+write.table(tidy, "tidy.txt", row.names = FALSE, quote = FALSE)
